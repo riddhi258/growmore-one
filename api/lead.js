46dc123a63274bec02e2a46db3dbd3b1337ea91d
USER-28 <USER-28@growmore.local>
2026-02-20 16:09:41 +0530

    update the changes

--- api/lead.js
+++ api/lead.js
@@ -16,7 +16,7 @@ export default async function handler(req, res) {
 
   try {
     const response = await fetch(
-      "https://case.growmore.one/add/company-website",
+      "https://case.growmore.one/admin/",
       {
         method: "POST",
         headers: {
