dc12998f210c4b9f54295b5fa6cddbd7ab2e4428
USER-28 <USER-28@growmore.local>
2026-02-23 09:51:31 +0530

    code updated

--- src/Pages/PointsCalculator.jsx
+++ src/Pages/PointsCalculator.jsx
@@ -9,9 +9,7 @@ import SpecialEduStep from "../Components/SpecialEduStep";
 import Credit from "../Components/Credit";
 import ScorePanel from "../Components/ScorePanel";
 
-
 export default function PointsCalculator() {
-
   // step controller
   const [step, setStep] = useState(1);
 
@@ -27,40 +25,114 @@ export default function PointsCalculator() {
     specialist: 0,
     community: 0,
     partner: 0,
-    professional: 0
+    professional: 0,
   });
 
   const updateScore = (key, value) => {
-    setScores(prev => ({
+    setScores((prev) => ({
       ...prev,
-      [key]: value
+      [key]: value,
     }));
   };
 
-  const nextStep = () => setStep(prev => prev + 1);
-  const prevStep = () => setStep(prev => prev - 1);
+  const nextStep = () => setStep((prev) => prev + 1);
+  const prevStep = () => setStep((prev) => prev - 1);
 
   const totalScore = Object.values(scores).reduce((a, b) => a + b, 0);
 
   return (
-    <div className="calculator-container">
-
-      {/* LEFT FORM */}
-<div className="form-panel">
-
-  {step === 1 && <VisaStep updateScore={updateScore} nextStep={nextStep} />}
-  {step === 2 && <AgeStep updateScore={updateScore} nextStep={nextStep} prevStep={prevStep} />}
-  {step === 3 && <EnglishStep updateScore={updateScore} nextStep={nextStep} prevStep={prevStep} />}
-  {step === 4 && <OverseasStep updateScore={updateScore} nextStep={nextStep} prevStep={prevStep} />}
-  {step === 5 && <AustralianStep updateScore={updateScore} nextStep={nextStep} prevStep={prevStep} />}
-  {step ===6 && <EducationStep updateScore={updateScore} nextStep={nextStep} prevStep={prevStep} />}
-  {step === 7 && <SpecialEduStep updateScore={updateScore} nextStep={nextStep} prevStep={prevStep} />}
-  {step === 8 && <Credit updateScore={updateScore} nextStep={nextStep} prevStep={prevStep} />}
-
-</div>
-      {/* RIGHT PANEL */}
-      <ScorePanel scores={scores} total={totalScore} />
-
+    <div className="w-full">
+      {/* ================= HEADER BANNER ================= */}
+      <section className="bg-[#28535B] py-16 md:py-20">
+        <div className="max-w-7xl mx-auto px-6 md:px-16">
+          <h1 className="text-3xl md:text-5xl font-semibold text-white mb-3">
+            Points Calculator
+          </h1>
+          <p className="text-white font-semibold">
+            Home &gt; Points Calculator
+          </p>
+        </div>
+      </section>
+      <div className="calculator-container">
+        {/* LEFT FORM */}
+        <div className="form-panel">
+          {step === 1 && (
+            <VisaStep updateScore={updateScore} nextStep={nextStep} />
+          )}
+          {step === 2 && (
+            <AgeStep
+              updateScore={updateScore}
+              nextStep={nextStep}
+              prevStep={prevStep}
+            />
+          )}
+          {step === 3 && (
+            <EnglishStep
+              updateScore={updateScore}
+              nextStep={nextStep}
+              prevStep={prevStep}
+            />
+          )}
+          {step === 4 && (
+            <OverseasStep
+              updateScore={updateScore}
+              nextStep={nextStep}
+              prevStep={prevStep}
+            />
+          )}
+          {step === 5 && (
+            <AustralianStep
+              updateScore={updateScore}
+              nextStep={nextStep}
+              prevStep={prevStep}
+            />
+          )}
+          {step === 6 && (
+            <EducationStep
+              updateScore={updateScore}
+              nextStep={nextStep}
+              prevStep={prevStep}
+            />
+          )}
+          {step === 7 && (
+            <SpecialEduStep
+              updateScore={updateScore}
+              nextStep={nextStep}
+              prevStep={prevStep}
+            />
+          )}
+          {step === 8 && (
+            <Credit
+              updateScore={updateScore}
+              nextStep={nextStep}
+              prevStep={prevStep}
+            />
+          )}
+        </div>
+        {/* RIGHT PANEL */}
+        <ScorePanel scores={scores} total={totalScore} />
+      </div>
+      <div className="max-w-7xl mx-auto px-4 text-justify md:px-16 py-20">
+        <p>
+          This New Point Test method is designed to help you determine
+          eligibility for skilled migration to Australia. This testing mainly
+          assesses your age, work experience, educational qualifications, and
+          English language ability. In this, your total points will be summed
+          and compared to the number of points required to qualify for the visa.
+        </p>
+        <p className="mt-4">
+          Understanding the New Point Test System Australia is crucial for all
+          applicants in the Skilled Migration program. This method will assist
+          applicants in understanding their overall status and potential areas
+          to improve to enhance the overall score and increase the opportunity
+          for success.
+        </p>
+        <p className="mt-4">
+          So, use our calculator now and get a clear idea of your eligibility
+          under the new system, and if you need any personalized advice, contact
+          our expert Immigration Agent today!
+        </p>
+      </div>
     </div>
   );
 }
