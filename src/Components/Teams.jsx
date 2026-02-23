60e2eeb5e81a1052bc69690cfa0b8f9bf94dc480
USER-28 <USER-28@growmore.local>
2026-02-23 08:48:44 +0530

    code updated

--- src/Components/Teams.jsx
+++ src/Components/Teams.jsx
@@ -26,28 +26,25 @@ const team = [
 const Teams = () => {
   return (
     <div className="w-full bg-white py-24">
-
       {/* ================= TEAM SECTION ================= */}
-      <div className="max-w-3xl mx-auto px-6">
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-10 justify-items-center">
+      <div className="max-w-4xl mx-auto px-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-10">
           {team.map((member, index) => (
             <div
               key={index}
-              className="bg-white rounded-2xl shadow-lg hover:shadow-2xl transition duration-300 overflow-hidden w-[300px]"
+              className="bg-white rounded-2xl shadow-lg hover:shadow-2xl transition duration-300 overflow-hidden w-full max-w-sm"
             >
               <img
                 src={member.img}
                 alt={member.name}
-                className="w-full h-[320px] object-cover"
+                className="w-full h-[500px] object-cover"
               />
 
               <div className="p-5">
                 <h3 className="text-[#1E4E54] font-semibold text-md">
                   {member.name}
                 </h3>
-                <p className="text-gray-500 text-sm mt-1">
-                  {member.role}
-                </p>
+                <p className="text-gray-500 text-sm mt-1">{member.role}</p>
               </div>
             </div>
           ))}
@@ -55,36 +52,55 @@ const Teams = () => {
       </div>
 
       {/* ================= COMMUNITY SECTION ================= */}
-      <div className="max-w-4xl mx-auto mt-28 px-6">
-        <div className="bg-[#1E4E54] rounded-[28px] p-8 md:p-10 flex flex-col md:flex-row items-center gap-10">
-
+      <div className="max-w-screen rounded-lg  mx-auto mt-28 px-6">
+            <div className="bg-[#1E4E54] rounded-[28px] p-8 md:p-10 flex flex-col md:flex-row items-center justify-center gap-10">
           {/* LEFT CARD */}
-          <div className="bg-white rounded-2xl p-8 w-full md:w-1/2">
-            <h3 className="text-lg font-semibold text-[#2CA6B0] mb-4">
+          <div className="bg-white rounded-2xl p-8 w-full h-[300px] md:w-3/4">
+            <h3 className="text-3xl font-semibold text-[#2CA6B0] mb-4 mt-4 pl-4">
               Community
             </h3>
 
-            <p className="text-gray-600 text-sm leading-relaxed mb-6">
+            <p className="text-gray-600 text-base leading-relaxed mb-6 text-justify px-4">
               Stay updated with the latest news, tips, and success stories by
-              connecting with us on our social media platforms.
+              connecting with us on our social media platforms:
             </p>
 
             {/* SOCIAL ICONS */}
-            <div className="flex gap-4">
+            <div className="flex gap-4 pt-6 px-4">
               <a href="#">
-                <img src="/assets/t.svg" alt="Tiktok" className="w-9 h-9 rounded-md  flex items-center justify-center-4" />
-              </a>
-             <a href="#">
-                <img src="/assets/i.svg" alt="Instagram" className="w-9 h-9 rounded-md  flex items-center justify-center" />
-              </a>
-              <a href="#">
-                <img src="/assets/t.svg" alt="Tiktok" className="w-9 h-9 rounded-md  flex items-center justify-center-4" />
-              </a>
-              <a href="#" >
-                <img src="/assets/l.svg" alt="LinkedIn" className="w-9 h-9 rounded-mdflex items-center justify-center" />
+                <img
+                  src="/assets/t.svg"
+                  alt="Tiktok"
+                  className="w-9 h-9 rounded-md  flex items-center justify-center-4"
+                />
               </a>
               <a href="#">
-                <img src="/assets/y.svg" alt="YouTube" className="w-9 h-9 rounded-lg  flex items-center justify-center" />
+                <img
+                  src="/assets/i.svg"
+                  alt="Instagram"
+                  className="w-9 h-9 rounded-md  flex items-center justify-center"
+                />
+              </a>
+              <a href="#">
+                <img
+                  src="/assets/t.svg"
+                  alt="Tiktok"
+                  className="w-9 h-9 rounded-md  flex items-center justify-center-4"
+                />
+              </a>
+              <a href="#">
+                <img
+                  src="/assets/l.svg"
+                  alt="LinkedIn"
+                  className="w-9 h-9 rounded-mdflex items-center justify-center"
+                />
+              </a>
+              <a href="#">
+                <img
+                  src="/assets/y.svg"
+                  alt="YouTube"
+                  className="w-9 h-9 rounded-lg  flex items-center justify-center"
+                />
               </a>
             </div>
           </div>
@@ -94,10 +110,9 @@ const Teams = () => {
             <img
               src="/assets/c.png"
               alt="community"
-              className="rounded-2xl w-full h-[260px] object-cover"
+              className="rounded-2xl w-full h-[320px] object-cover"
             />
           </div>
-
         </div>
       </div>
     </div>
