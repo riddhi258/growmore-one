ede9e314b7d8d51b48b5bb9613bbb5385c4a6b44
USER-28 <USER-28@growmore.local>
2026-02-25 14:29:35 +0530

    faq added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,6 +3,7 @@ import Hero from '../Components/Hero'
 import Card from '../Components/Card'
 import Famliy from '../Components/Famliy'
 import Labour from '../Components/Labour'
+import FAQ from '../Components/FAQ'
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
     <Card />
     <Famliy />
     <Labour/>
+    <FAQ/>
     </div>
   )
 }
