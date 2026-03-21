103b0c5421144e8e2817ef6adfe13ddb72117899
USER-28 <USER-28@growmore.local>
2026-03-21 09:36:23 +0530

    updated the code

--- api/gsm.js
+++ api/gsm.js
@@ -37,16 +37,13 @@ export default async function handler(req, res) {
       body: crmBody.toString(),
     }).catch(err => console.error("CRM Error:", err));
 
-   console.log("User:", process.env.EMAIL_USER ? "Loaded" : "MISSING");
-console.log("Pass:", process.env.EMAIL_PASS ? "Loaded" : "MISSING");
-
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
+  const transporter = nodemailer.createTransport({
+        service: "gmail",
+          auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+      });
     const emailHtml = `
       <div style="font-family: sans-serif; line-height: 1.6; color: #333;">
         <h2 style="color: #28535B;">New GSM Assessment Lead</h2>
