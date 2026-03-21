c11a20bca6f39c3eff63f42bdf593212e424f4f0
USER-28 <USER-28@growmore.local>
2026-03-21 09:41:48 +0530

    Purge all cached files and apply .gitignore

--- src/Components/StepHeader.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-export default function StepHeader({ title, points }) {
-  return (
-    <div className="step-header">
-      <h2>{title}</h2>
-      <div className="step-points">{points}</div>
-    </div>
-  );
-}
